bd1a8277e0d74161c77856da16b2bdd6fdafad9e
DarckOzzy <cesarjavierortizmontero@gmail.com>
2022-02-10 18:11:00 -0500

    Prepare to deploy

--- front_end/config.ts
+++ front_end/config.ts
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:3000/api/products`;
-export const prodEndpoint = `fill me in when we deploy`;
+export const prodEndpoint = 'https://riqraapp.herokuapp.com/api/products';
